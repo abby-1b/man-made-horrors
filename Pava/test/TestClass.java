39433a2515047e2b3c6f1b60105f369b61394160
CodeIGuess <codeiguess@gmail.com>
2023-02-12 23:06:31 -0400

    horrot

--- Pava/test/TestClass.java
+++ Pava/test/TestClass.java
@@ -1,12 +1,13 @@
-;
-;class TestClass
-	{public static void main(String[] args)
-		{String[] strs = {
-			"Str1",
-			"Str2",
-			"Str3"
+;;
+class TestClass {
+	public static void main(String[] args) {
+		String[] strs = {
+				"Str1",
+				"Str2",
+				"Str3"
 		};
-		;for (int a = 0; a < strs.length; a++)
-			{System.out.println(strs[a])
-;}}}
-; 
+		for (int a = 0; a < strs.length; a++) {
+			System.out.println(strs[a]);
+		}
+	}
+};
